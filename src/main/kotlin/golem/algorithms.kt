24bc19d77608ab765d334385342b9fb3048b19b8
kyonifer <kyle.j.kauffman@gmail.com>
2015-09-26 01:48:12 -0400

    Remove expm from top level funcs in prep for solutions in matrix interface.

--- src/main/kotlin/golem/algorithms.kt
+++ src/main/kotlin/golem/algorithms.kt
@@ -5,16 +5,3 @@
 
 package golem
 
-import golem.matrix.Matrix
-import golem.matrix.jblas.MatrixJBlas
-
-fun expm(F: Matrix<*,*>, t: Double = 1.0): Matrix<*,*>
-{
-    when (F) {
-        is MatrixJBlas -> {
-            var innermat = F.storage
-	    return MatrixJBlas(org.jblas.MatrixFunctions.expm(innermat))
-        }
-	else -> throw Exception("Unsupported matrix type for expm")
-    }
-}
