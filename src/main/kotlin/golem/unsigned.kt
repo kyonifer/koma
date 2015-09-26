c5d0ca36940def89d7c2b5e7eeb2a0ce36d016d0
kyonifer <kyle.j.kauffman@gmail.com>
2015-09-26 18:04:11 -0400

    Add unsigned type for uint8.

---
+++ src/main/kotlin/golem/unsigned.kt
@@ -0,0 +1,34 @@
+package golem
+
+class UInt8
+{
+    constructor(init: Short)
+    {
+        value = init.toByte()
+
+    }
+    constructor(init: Byte)
+    {
+        value = init
+    }
+    var value: Byte
+
+    fun plus(other: UInt8): UInt8
+    {
+        return UInt8((this.value + other.value).toByte())
+
+    }
+    fun minus(other: UInt8): UInt8
+    {
+        return UInt8((this.value - other.value).toByte())
+    }
+    fun divide(other: UInt8): UInt8
+    {
+        return UInt8(Integer.divideUnsigned(value.toInt(), other.value.toInt()).toByte())
+    }
+    fun times(other: UInt8): UInt8
+    {
+        return UInt8((value*other.value).toByte())
+    }
+    fun read(): Int = value.toInt() and 0xFF
+}
