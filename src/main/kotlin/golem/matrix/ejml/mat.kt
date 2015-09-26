3f1534dc1a471cb26bafb8cda4c34ea349175dbc
kyonifer <kyle.j.kauffman@gmail.com>
2015-09-26 01:50:26 -0400

    Rename EJML matrix file and first implementations of setCol, setRow and expm (expm is currently non functional). Also optimize factory referencing to not generate new object each call (use private module static).

--- src/main/kotlin/golem/matrix/ejml/mat.kt
+++ src/main/kotlin/golem/matrix/ejml/mat.kt
@@ -1,5 +1,8 @@
 package golem.matrix.ejml
 
+import golem.ceil
+import golem.logb
+import golem.pow
 import golem.matrix.Matrix
 import golem.matrix.ejml.backend.*
 import org.ejml.simple.SimpleMatrix
@@ -50,9 +53,131 @@ class Mat(var storage: SimpleMatrix) : Matrix<Mat, Double>
     override fun elementSum() = this.storage.elementSum()
     override fun trace() = this.storage.trace()
 
+    override fun setCol(index: Int, col: Mat) {
+        for (i in 0..col.numRows()-1)
+            this[i,index] = col[i]
+    }
+
+    override fun setRow(index: Int, row: Mat) {
+        for (i in 0..row.numCols()-1)
+            this[index, i] = row[i]
+    }
+
+    override fun getFactory() = golem.matrix.ejml.factory
     override val T: Mat
         get() = this.transpose()
 
+    override fun solve(A: Mat, B: Mat): Mat {
+        var out = this.getFactory().zeros(A.numCols(), 1)
+        CommonOps.solve(A.storage.matrix, B.storage.matrix, out.storage.matrix)
+        return out
+    }
+
+    override fun expm(): Mat {
+        var A = this
+        var A_L1 = org.ejml.ops.NormOps.inducedP1(A.storage.matrix)
+        var n_squarings = 0.0
+
+        if (A_L1 < 1.495585217958292e-002) {
+            val (U, V) = _pade3(A)
+            return dispatchPade(U,V, n_squarings)
+        }
+        else if (A_L1 < 2.539398330063230e-001) {
+            val (U, V) = _pade5(A)
+            return dispatchPade(U,V, n_squarings)
+        }
+        else if (A_L1 < 9.504178996162932e-001) {
+            val (U, V) = _pade7(A)
+            return dispatchPade(U,V, n_squarings)
+        }
+        else if (A_L1 < 2.097847961257068e+000) {
+            val (U, V) = _pade9(A)
+            return dispatchPade(U,V, n_squarings)
+        }
+        else {
+
+            var maxnorm = 5.371920351148152
+            n_squarings = golem.max(0.0, ceil(logb(2, A_L1 / maxnorm))) //
+            A = A / pow(2.0,n_squarings)
+            val (U, V) = _pade13(A)
+            return dispatchPade(U, V, n_squarings)
+        }
+    }
+    private fun dispatchPade(U: Mat, V: Mat, n_squarings: Double): Mat
+    {
+        var P = U+V
+        var Q = -U+V
+        //var R = solve(Q,P)
+        var R = getFactory().zeros(Q.numCols(), P.numCols())
+        for (i in 0..P.numCols()-1) {
+            R.setCol(i, solve(Q, P.getCol(i)))
+
+        }
+        for (i in 0..n_squarings-1)
+            R = R*R
+        return R
+    }
+
+    private fun _pade3(A: Mat): Pair<Mat, Mat>
+    {
+        var b = golem.mat[120, 60, 12, 1]
+        var ident = getFactory().eye(A.numRows(), A.numCols())
+
+        var A2 = A*A
+        var U = A*(A2*b[3]+ident*b[1])
+        var V = A2*b[2] + ident*b[0]
+
+        return Pair(U,V)
+    }
+    private fun _pade5(A: Mat): Pair<Mat, Mat>
+    {
+        var b = golem.mat[30240, 15120, 3360, 420, 30, 1]
+        var ident = A.getFactory().eye(A.numRows(), A.numCols())
+        var A2 = A*A
+        var A4 = A2*A2
+        var U = A*(A4*b[5]+A2*b[3]+ident*b[1])
+        var V = A4*b[4]+A2*b[2]+ident*b[0]
+        return Pair(U,V)
+
+    }
+    private fun _pade7(A: Mat): Pair<Mat, Mat>
+    {
+        var b = golem.mat[17297280, 8648640, 1995840, 277200, 25200, 1512, 56, 1]
+        var ident = A.getFactory().eye(A.numRows(), A.numCols())
+        var A2 = A*A
+        var A4 = A2*A2
+        var A6 = A4*A2
+        var U = A*(A6*b[7]+A4*b[5]+A2*b[3]+ident*b[1])
+        var V = A6*b[6]+A4*b[4]+A2*b[2]+ident*b[0]
+        return Pair(U,V)
+    }
+    private fun _pade9(A: Mat): Pair<Mat, Mat>
+    {
+        var b = golem.mat[17643225600, 8821612800, 2075673600, 302702400, 30270240,
+                2162160, 110880, 3960, 90, 1]
+        var ident = getFactory().eye(A.numRows(), A.numCols())
+        var A2 = A*A
+        var A4 = A2*A2
+        var A6 = A4*A2
+        var A8 = A6*A2
+        var U = A*(A8*b[9] + A6*b[7] + A4*b[5] + A2*b[3] + ident*b[1])
+        var V = A8*b[8] + A6*b[6] + A4*b[4] + A2*b[2] + ident*b[0]
+        return Pair(U,V)
+    }
+    private fun _pade13(A: Mat): Pair<Mat, Mat>
+    {
+        var b = golem.mat[64764752532480000, 32382376266240000, 7771770303897600,
+                1187353796428800, 129060195264000, 10559470521600, 670442572800,
+                33522128640, 1323241920, 40840800, 960960, 16380, 182, 1]
+        var ident = A.getFactory().eye(A.numRows(), A.numCols())
+
+        var A2 = A*A
+        var A4 = A2*A2
+        var A6 = A4*A2
+        var U = A*(A6*(A6*b[13] + A4*b[11] + A2*b[9]) + A6*b[7] + A4*b[5] + A2*b[3] + ident*b[1])
+        var V = A6*(A6*b[12] + A4*b[10] + A2*b[8]) + A6*b[6] + A4*b[4] + A2*b[2] + ident*b[0]
+        return Pair(U,V)
+    }
 
     override fun LU(): Pair<Mat, Mat> {
         val decomp = this.storage.LU()
@@ -109,5 +234,7 @@ class Mat(var storage: SimpleMatrix) : Matrix<Mat, Double>
                 "Please call getDouble and cast manually if this is intentional.")
     }
 
-
 }
+
+internal var factory: MatFactory = MatFactory()
+
