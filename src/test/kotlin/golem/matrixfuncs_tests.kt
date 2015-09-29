e64df73e2d77fc656993c2ab058e3d6965495d7f
kyonifer <kyle.j.kauffman@gmail.com>
2015-09-29 00:34:38 -0400

    Add expm tests.

---
+++ src/test/kotlin/golem/matrixfuncs_tests.kt
@@ -0,0 +1,18 @@
+package golem
+
+import org.junit.Test
+
+class matrixfuncs_tests {
+    @Test
+    fun testExpm()
+    {
+        assert(abs(cumsum(expm(eye(3)) - E*eye(3))) < 1e-9)
+
+        val expected = mat[20.08553692318771,   0,                 0 end
+                0,                   20.08553692318771, 0 end
+                0,                   0,                 20.08553692318771]
+        assert(abs(cumsum(expm(eye(3)*3) - expected)) < 1e-9)
+
+        assert(false)
+    }
+}
