3e5db09a8904e938fabe8ac6b6fb0b0e3e32a780
kyonifer <kyle.j.kauffman@gmail.com>
2015-09-29 00:40:09 -0400

    Remove accidental assert.

--- src/test/kotlin/golem/matrixfuncs_tests.kt
+++ src/test/kotlin/golem/matrixfuncs_tests.kt
@@ -12,7 +12,5 @@ class matrixfuncs_tests {
                 0,                   20.08553692318771, 0 end
                 0,                   0,                 20.08553692318771]
         assert(abs(cumsum(expm(eye(3)*3) - expected)) < 1e-9)
-
-        assert(false)
     }
 }
