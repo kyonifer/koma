ea9a83fdaec4b0564bffc218e62ba8b17c37c428
Dan <git@drmoose.net>
2016-03-26 11:22:31 -0400

    Add matrix validaiton feature.

--- src/main/golem/options.kt
+++ src/main/golem/options.kt
@@ -20,3 +20,9 @@ import golem.matrix.*
  *
  */
 var factory: MatrixFactory<Matrix<Double>> = golem.matrix.mtj.MTJMatrixFactory()
+
+/**
+ * Whether to validate the dimensions, symmetry, and values of input matrices. false is faster, and should be
+ * used for realtime applications. true gives you much more useful errors when your matrices are shaped
+ * differently than your code expects. */
+var validateMatrices = true
