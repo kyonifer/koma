6237cfb0ab7bc668f871deb568a7e8436a4a2f65
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2016-04-20 13:21:26 -0400

    Make default factory settable without assuming a specific backend is available.

--- src/main/golem/options.kt
+++ src/main/golem/options.kt
@@ -11,6 +11,8 @@ package golem
 
 import golem.matrix.*
 
+
+private var _factory: MatrixFactory<Matrix<Double>>? = null
 /**
  *
  * Default factory that all top-level functions use when building new matrices.
@@ -19,7 +21,38 @@ import golem.matrix.*
  * backend the top-level functions use for computation.
  *
  */
-var factory: MatrixFactory<Matrix<Double>> = golem.matrix.mtj.MTJMatrixFactory()
+var factory: MatrixFactory<Matrix<Double>>
+    get() {
+        var facInst = _factory
+        if (facInst != null) {
+            return facInst
+        }
+        else {
+            // At runtime, see which backends are on our classpath (if any)
+            var facs = arrayOf("golem.matrix.mtj.MTJMatrixFactory",
+                               "golem.matrix.ejml.EJMLMatrixFactory")
+            facs.forEach {
+                try {
+                    var fac = Class.forName(it)
+                    fac.constructors.forEach { ctor ->
+                        if (ctor.parameterTypes.isEmpty()) {
+                            @Suppress("UNCHECKED_CAST")
+                            var out = ctor.newInstance() as MatrixFactory<Matrix<Double>>
+                            _factory = out
+                            return out
+                        }
+                    }
+                } catch(e: ClassNotFoundException) {
+                }
+
+            }
+            throw RuntimeException("No default backends for golem matrix found. Please set golem.factory manually or" +
+                                   " put one on your classpath.")
+        }
+    }
+    set(factory: MatrixFactory<Matrix<Double>>) {
+        _factory = factory
+    }
 
 /**
  * Whether to validate the dimensions, symmetry, and values of input matrices. false is faster, and should be
