dce89c7dc42b65dab365b43bba8c20079a946e87
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2016-05-03 02:04:13 -0400

    Remove main.

--- src/main/Main.kt
+++
@@ -1,8 +0,0 @@
-@file:JvmName("NoEntryPoint")
-
-/**
- * An entry point in case the library is accidentally run as an executable.
- */
-fun main(args: Array<String>) {
-    println("golem is a library not an executable")
-}
