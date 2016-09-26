cb4908f8b6e96dd4fed573f5edd10dd75e2dccc1
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2016-09-26 17:26:39 -0400

    Fix signum support on js.

--- golem-core/srcjs/golem/platformsupport/signum.kt
+++ golem-core/srcjs/golem/platformsupport/signum.kt
@@ -6,16 +6,7 @@ package golem.platformsupport
  * we can't add these directly to MathClass because the name clashes
  * with kotlin-js-library.
  */
-@native
-class ExtraMathClass() {
-    // Our 2 functions
-    fun signum(value: Double): Double {
-        val m: dynamic = Math
-        return m.sign(value)
-    }
+fun signum(value: Double): Double {
+    val m: dynamic = Math
+    return m.sign(value)
 }
-
-@native
-val ExtraMath: ExtraMathClass = ExtraMathClass()
-
-fun signum(value: Double) = ExtraMath.signum(value)
