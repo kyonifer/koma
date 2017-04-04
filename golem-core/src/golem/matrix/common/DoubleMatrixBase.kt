0da5ad9e5179e553f0c063ef46e89ee4017eb438
Unknown <kyle.j.kauffman@gmail.com>
2017-04-04 02:09:50 -0400

    Move out generic functions to matrix base.

--- golem-core/src/golem/matrix/common/DoubleMatrixBase.kt
+++ golem-core/src/golem/matrix/common/DoubleMatrixBase.kt
@@ -9,38 +9,8 @@ import golem.matrix.*
  * Some functionality to help more easily implement double based golem backends. Feel free to not use if
  * your backend has fast implementations of these functions.
  */
-abstract class DoubleMatrixBase : Matrix<Double> {
+abstract class DoubleMatrixBase : MatrixBase<Double>() {
 
-    /**
-     * Attempts to downcast a matrix to its specific subclass,
-     * accepting both inner wrapped types and outer types.
-     * Requires the [TOuter] constructor to be passed in because
-     * reified generics don't support ctor calls. If the passed
-     * mat cannot be cast, instead copies the data manually into
-     * a newly allocated matrix of the correct type.
-     */
-    protected inline fun
-            <reified TOuter: Matrix<Double>, reified TInner>
-            castOrCopy(mat: Matrix<Double>,
-                       makeOuter: (TInner) -> TOuter,
-                       outerFac: MatrixFactory<TOuter>): TOuter {
-
-        when (mat) {
-            is TOuter -> return mat
-            else      -> {
-                val base = mat.getBaseMatrix()
-                if (base is TInner)
-                    return makeOuter(base)
-                else {
-                    val out = outerFac.zeros(mat.numRows(), mat.numCols())
-                    for (row in 0.until(mat.numRows()))
-                        for (col in 0.until(mat.numCols()))
-                            out[row, col] = mat[row, col]
-                    return out
-                }
-            }
-        }
-    }
 
     override fun getDouble(i: Int, j: Int) = this[i, j]
     override fun getDouble(i: Int) = this[i]
---
+++ golem-core/src/golem/matrix/common/MatrixBase.kt
@@ -0,0 +1,36 @@
+package golem.matrix.common
+
+import golem.matrix.*
+
+abstract class MatrixBase<T>: Matrix<T> {
+    /**
+     * Attempts to downcast a matrix to its specific subclass,
+     * accepting both inner wrapped types and outer types.
+     * Requires the [TOuter] constructor to be passed in because
+     * reified generics don't support ctor calls. If the passed
+     * mat cannot be cast, instead copies the data manually into
+     * a newly allocated matrix of the correct type.
+     */
+    protected inline fun
+            <DType, reified TOuter: Matrix<DType>, reified TInner>
+            castOrCopy(mat: Matrix<DType>,
+                       makeOuter: (TInner) -> TOuter,
+                       outerFac: MatrixFactory<TOuter>): TOuter {
+
+        when (mat) {
+            is TOuter -> return mat
+            else      -> {
+                val base = mat.getBaseMatrix()
+                if (base is TInner)
+                    return makeOuter(base)
+                else {
+                    val out = outerFac.zeros(mat.numRows(), mat.numCols())
+                    for (row in 0.until(mat.numRows()))
+                        for (col in 0.until(mat.numCols()))
+                            out[row, col] = mat[row, col]
+                    return out
+                }
+            }
+        }
+    }
+}
