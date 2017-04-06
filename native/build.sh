a97b1df9f27b2238f20d3234e00f42fcb9a19b27
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2017-04-06 10:59:44 -0400

    Remove debug symbols and optimize by default.

--- native/build.sh
+++ native/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cinterop -def ./Math.def -o Math.kt.bc
 cinterop -def ./C.def -o C.kt.bc
-kotlinc-native golem-backend-purekt/src golem-core/src golem-core/srcnative main.kt -library Math.kt.bc -library C.kt.bc
+kotlinc-native golem-backend-purekt/src golem-core/src golem-core/srcnative main.kt -library Math.kt.bc -library C.kt.bc -linkerArgs -S -opt
+strip program.kexe
 
