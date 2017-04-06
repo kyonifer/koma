10ad77c0c28afe299eb951c8aa94478151025597
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2017-04-06 10:30:34 -0400

    Add working demo for native.

--- native/main.kt
+++ native/main.kt
@@ -1,5 +1,21 @@
 import golem.*
+import golem.matrix.purekt.*
 
 fun main(args: Array<String>) {
-	golem.zeros(3,3)
+    
+    factory = DoublePureKtMatrixFactory()
+    
+    println("randn(5,6): \n${randn(5,6).repr()}")
+
+    var a = mat[1,2,3 end
+                4,5,6]
+    
+    var b = mat[2, 0 end
+                0, 1 end
+               -1, 5]
+    
+    println("a: \n${a.repr()}")
+    println("b: \n${b.repr()}")
+    println("a*b: \n${(a*b).repr()}")
+    
 }
