e7ef385ea2c84931d5a2f5121e5b408c79f4379d
Unknown <kyle.j.kauffman@gmail.com>
2017-04-17 03:25:55 -0400

    Better js example.

--- js/example.js
+++ js/example.js
@@ -1,7 +1,16 @@
-g = require('golem-core')
+golem = require('golem-core').golem
 
-m = g.golem.randn(3,3)
+m = golem.randn(3,3)
 
 console.log(m)
 console.log(m.plus(m.timesScalar(5)))
- 
+
+// Force a particular backend
+
+Fac = require('golem-backend-purekt').golem.matrix.purekt.DoublePureKtMatrixFactory
+f = new Fac()
+
+m2 = f.ones(3,4)
+
+console.log(m2)
+console.log(m2.plus(m2).minusScalar(3))
