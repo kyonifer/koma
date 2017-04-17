1a0c80cb33f5bef75634c86baf734e117752e6d8
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2017-04-17 00:03:42 -0400

    Move code that only compiles on the jvm to srcjvm.

--- js/prepare.sh
+++ js/prepare.sh
@@ -2,5 +2,3 @@
 
 find ../golem-core ../golem-backend-purekt -type f | xargs sed -i 's/ Math\./ kotlin.js.Math./g'
 find ../golem-core -type f | xargs sed -i '/String.format/d'
-
-rm golem-core/src/golem/plot.kt
