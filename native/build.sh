0cbfaad9970aa95ccd53e20ef7b3b7199d87f48e
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2017-04-22 18:12:22 -0400

    Unify build system between the target platforms.

--- native/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cinterop -def ./Math.def -o Math.kt.bc
-cinterop -def ./C.def -o C.kt.bc
-kotlinc-native golem-backend-purekt/src golem-core/src golem-core/srcnative main.kt -library Math.kt.bc -library C.kt.bc -linkerArgs -S -opt
-strip program.kexe
-
