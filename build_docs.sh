52a6e87f6b8d10a507b8ddddd2bb8546e2efe67a
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2017-10-21 12:06:19 -0400

    Update build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# You need to set $DOKKA_FATJAR and build with 'gradle shadowJar' before running this.
-java -jar $DOKKA_FATJAR -classpath build/libs/koma-all.jar -include koma-core/src/Module.md -format javadoc koma-*/src
+./gradlew dokka
+pushd docs && mkdocs build && popd
