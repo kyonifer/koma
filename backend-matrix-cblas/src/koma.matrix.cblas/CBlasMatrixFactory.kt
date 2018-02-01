6e36c66cf0747cb24437f8c0c91e584f7c450482
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-02-01 17:48:41 -0500

    Fix native.

--- backend-matrix-cblas/src/koma.matrix.cblas/CBlasMatrixFactory.kt
+++ backend-matrix-cblas/src/koma.matrix.cblas/CBlasMatrixFactory.kt
@@ -11,9 +11,6 @@ class CBlasMatrixFactory: DoubleFactoryBase<CBlasMatrix>() {
     override fun zeros(rows: Int, cols: Int)
         = CBlasMatrix(rows, cols)
 
-    @Deprecated(DEPRECATE_IMPLICIT_2D, ReplaceWith("zeros(size, size)"))
-    override fun zeros(size: Int) = zeros(size, size)
-
     override fun create(data: IntRange): CBlasMatrix {
         val dataArray = fromCollection(data.map { it.toDouble() })
         return CBlasMatrix(1, dataArray.size).also {
@@ -45,11 +42,6 @@ class CBlasMatrixFactory: DoubleFactoryBase<CBlasMatrix>() {
                 it[row, col] = 1.0
             }
         }
-
-    @Deprecated(DEPRECATE_IMPLICIT_2D, ReplaceWith("ones(size, size)"))
-    override fun ones(size: Int): CBlasMatrix {
-        return ones(size, size)
-    }
 
     override fun eye(size: Int)
         = eye(size, size)
