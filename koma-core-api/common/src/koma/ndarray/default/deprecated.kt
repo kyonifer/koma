ec3a5acc645f48c5b4bb32590b1ed8b6295959fe
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-04-16 20:35:54 +0000

    Deprecate typealiases.

--- koma-core-api/common/src/koma/ndarray/default/deprecated.kt
+++ koma-core-api/common/src/koma/ndarray/default/deprecated.kt
@@ -1,10 +1,12 @@
 package koma.ndarray.default
 
-// These types have been moved to internal in anticipation of NDArray reorg.
-// However, as the reorg hasn't happened yet they aren't marked @Deprecated.
-// They should be shortly.
+import koma.ndarray.doubleFactory
 
+@Deprecated("Use toplevel factory koma.ndarray.getGenericFactory<T>().create()")
 typealias DefaultNDArray<T> = koma.internal.default.generated.ndarray.DefaultGenericNDArray<T>
+@Deprecated("Use toplevel factory koma.ndarray.intFactory.create()")
 typealias DefaultIntNDArray = koma.internal.default.generated.ndarray.DefaultIntNDArray
+@Deprecated("Use toplevel factory koma.ndarray.floatFactory.create()")
 typealias DefaultFloatNDArray = koma.internal.default.generated.ndarray.DefaultFloatNDArray
+@Deprecated("Use toplevel factory koma.ndarray.doubleFactory.create()")
 typealias DefaultDoubleNDArray = koma.internal.default.generated.ndarray.DefaultDoubleNDArray
