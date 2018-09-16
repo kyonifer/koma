411f03cd5fff6305810893a9d5b4f547312277aa
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-09-16 22:08:21 +0000

    Update docs building instructions.

--- build_docs.sh
+++ build_docs.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-set -e 
-
 # Requires pip installs of: mkdocs-bootswatch
+
+set -e
 
 ./gradlew dokka
 pushd docs && python3 -m mkdocs build && popd
+
+# Remote deploy:
+# pushd docs && python3 -m mkdocs gh-deploy && popd
+# git checkout gh-pages
+# echo "koma.kyonifer.com" > CNAME
+# git add CNAME && git commit --amend
+# git push origin gh-pages --force-with-lease
