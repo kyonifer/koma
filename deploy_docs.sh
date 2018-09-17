e155d6285574539ff93d885aebb7cfef69d40439
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-09-17 00:37:37 +0000

    Move deploy scripts.

---
+++ deploy_docs.sh
@@ -0,0 +1,7 @@
+./gradlew dokka
+pushd docs && python3 -m mkdocs gh-deploy && popd
+git checkout gh-pages
+echo "koma.kyonifer.com" > CNAME
+pygmentize -S default -f html -a .codehilite > code_styles.css
+git add CNAME code_styles.css && git commit --amend
+git push origin gh-pages --force-with-lease
