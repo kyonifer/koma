b1d7d6f1a2065dae3e5804ddcef30ef27b9b3312
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-09-17 00:28:49 +0000

    Another attempt.

--- build_docs.sh
+++ build_docs.sh
@@ -9,8 +9,11 @@ pushd docs && python3 -m mkdocs build && popd
 pygmentize -S default -f html -a .codehilite > docs/site/code_styles.css
 
 # Remote deploy:
+#
+# ./gradlew dokka
 # pushd docs && python3 -m mkdocs gh-deploy && popd
 # git checkout gh-pages
 # echo "koma.kyonifer.com" > CNAME
-# git add CNAME && git commit --amend
+# pygmentize -S default -f html -a .codehilite > code_styles.css
+# git add CNAME code_styles.css && git commit --amend
 # git push origin gh-pages --force-with-lease
