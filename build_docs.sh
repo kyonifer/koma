676520b72c364d14c7713d40eb15a5420d576325
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-09-17 00:04:48 +0000

    Attempt at fixing syntax highlighting.

--- build_docs.sh
+++ build_docs.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# Requires pip installs of: mkdocs-bootswatch
+# Requires pip installs of: mkdocs-bootswatch, mkdocs, pygments
 
 set -e
 
 ./gradlew dokka
 pushd docs && python3 -m mkdocs build && popd
+pygmentize -S default -f html -a .codehilite > docs/site/code_styles.css
 
 # Remote deploy:
 # pushd docs && python3 -m mkdocs gh-deploy && popd
