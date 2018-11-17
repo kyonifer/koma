a9e91aba9b67f14e4a01389fbbbf1589ff60633f
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2018-11-17 11:59:27 -0500

    Enable more native targets.

--- examples/native/main.kt
+++ examples/native/main.kt
@@ -56,11 +56,11 @@ fun main(args: Array<String>) {
 
     var a = mat[1,2,3 end
                 4,5,6]
-    
+
     var b = mat[2, 0 end
                 0, 1 end
                -1, 5]
-    
+
     println("a: \n${a.repr()}")
     println("b: \n${b.repr()}")
     println("a*b: \n${(a*b).repr()}")
